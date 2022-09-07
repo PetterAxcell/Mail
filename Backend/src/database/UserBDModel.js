a44fbe28026aa9a8d153bff845399ef194b8c017
Petter Axcell <petteraxcell03@gmail.com>
2022-09-07 12:20:00 +0200

    Added mysql with right tree folders

--- Backend/src/database/UserBDModel.js
+++ Backend/src/database/UserBDModel.js
@@ -1,5 +1,18 @@
+const { createConnection } = require('mysql');
+
+const pool = createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Ariannaj15',
+    database:'minimail',
+    connectionlimit:10
+})
+
 const addUserModel = (body) => {
-    console.log(body)
+    values = [body.name, body.surname, body.mail, body.pass]
+    var sql = "INSERT INTO users (Name, Lastname, Mail, Password) VALUES (?)";
+    pool.query(sql, [values], (err, res)=>{
+        if(err){return console.log(err);}})
     return 'DB';
 }
 
