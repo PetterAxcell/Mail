b419b9884196bd14da484aba9a90e33ddbeba8ab
Petter Axcell <petteraxcell03@gmail.com>
2022-09-09 13:22:51 +0200

    Css provisional

--- Frontend/src/Components/MUser.vue
+++ Frontend/src/Components/MUser.vue
@@ -1,16 +1,20 @@
 <template>
-    <div>
-        <h1 class="text-light">Mail</h1>
-        <ul> 
-            <li class="text-light" v-for="i in this.list_mails" :key="i.ID_user">
-                {{i.Subject}}
+    <div class =" bg-secondary">
+        <div class="p-3">
+            <h1 class="text-light">Mail</h1>
+            <hr  class="text-light" />
+        </div> <ul class = "list-unstyled p-3"> 
+            <li class="m-3 bg-light d-inline-block" v-for="i in this.list_mails" :key="i.ID_user">
+                <div class="d-inline-block">
+                    <img src="../../public/favicon.ico" alt="">
+                    <router-link to="/signup" class="p-3 d-inline-block "> {{i.From_user}} {{i.Subject}} </router-link>
+                </div>
             </li>
         </ul>
     </div>
 </template>
 <script>
     import axios from "axios"
-    import router from '../router'
 
     export default {
         name: 'MUser',
